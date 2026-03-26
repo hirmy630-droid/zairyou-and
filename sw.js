21e55e2a34407109c862927ad442e21138bc0ab2
hirmy630-droid <hirmy630@gmail.com>
2026-03-26 18:34:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,37 @@
-const CACHE_NAME = 'zairyo-complete-v1';
+const CACHE_NAME = 'zairyo-v2';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
   './icon-180.png',
   './icon-192.png',
-  './icon-512.png',
-  './icon-1024.png',
-  './splash-1170x2532.png'
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve())
-    )).then(() => self.clients.claim())
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) =>
+          key !== CACHE_NAME ? caches.delete(key) : Promise.resolve()
+        )
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
 
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  if (url.origin === location.origin) {
-    event.respondWith(
-      caches.match(event.request).then((cached) => cached || fetch(event.request))
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
 });
